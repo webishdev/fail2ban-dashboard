7e6cf052e0f4c593dccdfa1f8b7f1077574f6d0b
Simon Skoczylas <github@giftkugel.de>
2025-05-24 13:13:38 +0200

    Add version to build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 GIT_HASH=$(git rev-parse --short=11 HEAD)
+VERSION="${1:-development}"
 
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w -X 'main.Version=$VERSION' -X 'main.GitHash=$GIT_HASH'" ./cmd/fail2ban-dashboard
