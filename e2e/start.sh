3bb14bae0d6011533a2cd8551814f24d3de5d59d
Simon Skoczylas <github@giftkugel.de>
2025-05-24 17:10:06 +0200

    Enable other jail for E2E

--- e2e/start.sh
+++ e2e/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -p 3000:3000 -v ${PWD}:/files docker.io/library/fail2ban-dashboard /bin/bash
+docker run -it --rm -p 3000:3000 -v ${PWD}:/files -v ${PWD}/e2e/jail.conf:/etc/fail2ban/jail.conf docker.io/library/fail2ban-dashboard /bin/bash
