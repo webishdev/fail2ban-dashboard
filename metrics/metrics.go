1c50576f1db002f976ede15ad9a28887ff35852e
Simon Skoczylas <github@giftkugel.de>
2026-02-12 16:34:17 +0100

    Refactoring

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -16,82 +16,105 @@ type Configuration struct {
 	Version         string
 }
 
+type metrics struct {
+	reg                      *prometheus.Registry
+	versionInfoMetrics       *prometheus.GaugeVec
+	jailCountMetrics         prometheus.Gauge
+	jailBannedCurrentMetrics *prometheus.GaugeVec
+	jailFailedCurrentMetrics *prometheus.GaugeVec
+	jailBannedTotalMetrics   *prometheus.GaugeVec
+	jailFailedTotalMetrics   *prometheus.GaugeVec
+}
+
 func ServeMetrics(dataStore *store.DataStore, configuration *Configuration) {
 
-	var versionInfoMetrics = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "fail2ban_dashboard_info",
-			Help: "The fail2ban Dashboard build information",
-		},
-		[]string{"version", "fail2ban_version"},
-	)
-
-	var jailCountMetrics = prometheus.NewGauge(
-		prometheus.GaugeOpts{
-			Name: "f2b_jail_count",
-			Help: "The number of jails in fail2ban",
-		},
-	)
-
-	var jailBannedCurrentMetrics = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "f2b_jail_banned_current",
-			Help: "Amount of banned IPs currently in jail",
-		},
-		[]string{"jail"},
-	)
-
-	var jailFailedCurrentMetrics = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "f2b_jail_failed_current",
-			Help: "Amount of failed IPs currently in jail",
-		},
-		[]string{"jail"},
-	)
-
-	var jailBannedTotalMetrics = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "f2b_jail_banned_total",
-			Help: "Amount of banned IPs total in jail",
-		},
-		[]string{"jail"},
-	)
-
-	var jailFailedTotalMetrics = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "f2b_jail_failed_total",
-			Help: "Amount of failed IPs total in jail",
-		},
-		[]string{"jail"},
-	)
-
-	reg := prometheus.NewRegistry()
-	reg.MustRegister(versionInfoMetrics, jailCountMetrics, jailBannedCurrentMetrics, jailFailedCurrentMetrics, jailBannedTotalMetrics, jailFailedTotalMetrics)
-	versionInfoMetrics.WithLabelValues(configuration.Version, configuration.Fail2BanVersion).Set(1)
+	currentMetrics := setupRegistry()
+	currentMetrics.versionInfoMetrics.WithLabelValues(configuration.Version, configuration.Fail2BanVersion).Set(1)
 
 	log.Infof("Metrics enabled at %s", configuration.Address)
-	go listen(configuration.Address, reg)
+	go serve(configuration.Address, currentMetrics.reg)
 
+	updateMetrics(currentMetrics, dataStore)
+}
+
+func setupRegistry() *metrics {
+	result := &metrics{
+		reg: prometheus.NewRegistry(),
+		versionInfoMetrics: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "fail2ban_dashboard_info",
+				Help: "The fail2ban Dashboard build information",
+			},
+			[]string{"version", "fail2ban_version"},
+		),
+		jailCountMetrics: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Name: "f2b_jail_count",
+				Help: "The number of jails in fail2ban",
+			},
+		),
+		jailBannedCurrentMetrics: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "f2b_jail_banned_current",
+				Help: "Amount of banned IPs currently in jail",
+			},
+			[]string{"jail"},
+		),
+		jailFailedCurrentMetrics: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "f2b_jail_failed_current",
+				Help: "Amount of failed IPs currently in jail",
+			},
+			[]string{"jail"},
+		),
+		jailBannedTotalMetrics: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "f2b_jail_banned_total",
+				Help: "Amount of banned IPs total in jail",
+			},
+			[]string{"jail"},
+		),
+		jailFailedTotalMetrics: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "f2b_jail_failed_total",
+				Help: "Amount of failed IPs total in jail",
+			},
+			[]string{"jail"},
+		),
+	}
+
+	result.reg.MustRegister(result.versionInfoMetrics, result.jailCountMetrics, result.jailBannedCurrentMetrics, result.jailFailedCurrentMetrics, result.jailBannedTotalMetrics, result.jailFailedTotalMetrics)
+
+	return result
+}
+
+// dataStoreInterface defines the minimal interface needed for updateMetrics
+type dataStoreInterface interface {
+	RegisterUpdateHandler(handler store.UpdateHandler)
+	GetJails() []store.Jail
+}
+
+func updateMetrics(currentMetrics *metrics, dataStore dataStoreInterface) {
 	dataStore.RegisterUpdateHandler(func() {
 		log.Debug("Updating metrics")
 		jails := dataStore.GetJails()
 		jailCount := float64(len(jails))
-		jailCountMetrics.Set(jailCount)
+		currentMetrics.jailCountMetrics.Set(jailCount)
 		for _, jail := range jails {
 			numberOfBanned := float64(jail.CurrentlyBanned)
 			totalBanned := float64(jail.TotalBanned)
 			numberOfFailed := float64(jail.CurrentlyFailed)
 			totalFailed := float64(jail.TotalFailed)
-			jailBannedCurrentMetrics.WithLabelValues(jail.Name).Set(numberOfBanned)
-			jailBannedTotalMetrics.WithLabelValues(jail.Name).Set(totalBanned)
-			jailFailedCurrentMetrics.WithLabelValues(jail.Name).Set(numberOfFailed)
-			jailFailedTotalMetrics.WithLabelValues(jail.Name).Set(totalFailed)
+			currentMetrics.jailBannedCurrentMetrics.WithLabelValues(jail.Name).Set(numberOfBanned)
+			currentMetrics.jailBannedTotalMetrics.WithLabelValues(jail.Name).Set(totalBanned)
+			currentMetrics.jailFailedCurrentMetrics.WithLabelValues(jail.Name).Set(numberOfFailed)
+			currentMetrics.jailFailedTotalMetrics.WithLabelValues(jail.Name).Set(totalFailed)
 		}
 
 	})
 }
 
-func listen(address string, reg *prometheus.Registry) {
+func serve(address string, reg *prometheus.Registry) {
 	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
 
 	metricsError := http.ListenAndServe(address, nil)
